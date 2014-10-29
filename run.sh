b27da44fc63bb84b8bb452c0bee96e4b4939ab93
tcnksm <nsd22843@gmail.com>
2014-10-29 22:42:53 +0900

    Add new ghr and use --delete option

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ if [ "${PRE_RELEASE}" = "true" ]; then
 fi 
 
 if [ "${REPLACE}" = "true" ]; then
-    OPT="${OPT} --replace"
+    OPT="${OPT} --delete"
 fi 
 
 # If not version is not supplied,
@@ -31,6 +31,8 @@ if [ -z "${VERSION}" ]; then
 fi
 
 OWNER=${OWNER:-$WERCKER_GIT_OWNER}
+
+$WERCKER_STEP_ROOT/bin/ghr -v
 
 $WERCKER_STEP_ROOT/bin/ghr \
     -t ${GITHUB_TOKEN} \
